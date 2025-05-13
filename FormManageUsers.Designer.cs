1db404535c7940b348973b23c0db614e35ad9647
Rain141414 <rainierpaquibot70@gmail.com>
2025-05-13 20:51:06 +0800

    Add files via upload

---
+++ FormManageUsers.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace StockSyncManageUsers
+
+{
+    partial class FormManageUsers
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.clbPermissions = new System.Windows.Forms.CheckedListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblRole = new System.Windows.Forms.Label();
+            this.grpPermissions = new System.Windows.Forms.GroupBox();
+            this.grpPermissions.SuspendLayout();
+            this.SuspendLayout();
+
+            // txtName
+            this.txtName.Location = new System.Drawing.Point(100, 20);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 23);
+
+            // cmbRole
+            this.cmbRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRole.Location = new System.Drawing.Point(100, 60);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(200, 23);
+            this.cmbRole.SelectedIndexChanged += new System.EventHandler(this.cmbRole_SelectedIndexChanged);
+
+            // clbPermissions
+            this.clbPermissions.FormattingEnabled = true;
+            this.clbPermissions.Location = new System.Drawing.Point(10, 20);
+            this.clbPermissions.Name = "clbPermissions";
+            this.clbPermissions.Size = new System.Drawing.Size(260, 112);
+
+            // grpPermissions
+            this.grpPermissions.Controls.Add(this.clbPermissions);
+            this.grpPermissions.Location = new System.Drawing.Point(20, 100);
+            this.grpPermissions.Name = "grpPermissions";
+            this.grpPermissions.Size = new System.Drawing.Size(280, 140);
+            this.grpPermissions.Text = "Permissions";
+
+            // btnAdd
+            this.btnAdd.Location = new System.Drawing.Point(20, 260);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(80, 30);
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+
+            // btnUpdate
+            this.btnUpdate.Location = new System.Drawing.Point(120, 260);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(80, 30);
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+
+            // btnDelete
+            this.btnDelete.Location = new System.Drawing.Point(220, 260);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(80, 30);
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+
+            // lstUsers
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.ItemHeight = 15;
+            this.lstUsers.Location = new System.Drawing.Point(320, 20);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(250, 229);
+            this.lstUsers.SelectedIndexChanged += new System.EventHandler(this.lstUsers_SelectedIndexChanged);
+
+            // lblName
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(20, 23);
+            this.lblName.Name = "lblName";
+            this.lblName.Text = "Name:";
+
+            // lblRole
+            this.lblRole.AutoSize = true;
+            this.lblRole.Location = new System.Drawing.Point(20, 63);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Text = "Role:";
+
+            // FormManageUsers
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.ClientSize = new System.Drawing.Size(594, 311);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblRole);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.cmbRole);
+            this.Controls.Add(this.grpPermissions);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lstUsers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormManageUsers";
+            this.Text = "Manage Users";
+            this.grpPermissions.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.CheckedListBox clbPermissions;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox lstUsers;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.GroupBox grpPermissions;
+    }
+}
---
+++ FormManageUsers.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace StockSyncManageUsers
+{
+    public partial class FormManageUsers : Form
+    {
+        private class User
+        {
+            public string Name { get; set; }
+            public string Role { get; set; }
+            public List<string> Permissions { get; set; }
+        }
+
+        private List<User> users = new List<User>();
+
+        private readonly string[] allPermissions = {
+            "View Inventory",
+            "Edit Inventory",
+            "View Sales",
+            "Edit Sales",
+            "Generate Reports",
+            "Manage Users"
+        };
+
+        public FormManageUsers()
+        {
+            InitializeComponent();
+
+            cmbRole.Items.AddRange(new[] { "Owner", "User" });
+            cmbRole.SelectedIndex = 1; // default to User
+            clbPermissions.Items.AddRange(allPermissions);
+
+            UpdatePermissionVisibility();
+        }
+
+        private void UpdatePermissionVisibility()
+        {
+            clbPermissions.Enabled = cmbRole.SelectedItem?.ToString() == "User";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text.Trim();
+            string role = cmbRole.SelectedItem.ToString();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Please enter a name.");
+                return;
+            }
+
+            var newUser = new User
+            {
+                Name = name,
+                Role = role,
+                Permissions = new List<string>()
+            };
+
+            if (role == "User")
+            {
+                foreach (string permission in clbPermissions.CheckedItems)
+                    newUser.Permissions.Add(permission);
+            }
+            else
+            {
+                newUser.Permissions.Add("All Permissions");
+            }
+
+            users.Add(newUser);
+            RefreshUserList();
+            ClearForm();
+        }
+
+        private void RefreshUserList()
+        {
+            lstUsers.Items.Clear();
+            foreach (var user in users)
+                lstUsers.Items.Add($"{user.Name} ({user.Role})");
+        }
+
+        private void ClearForm()
+        {
+            txtName.Clear();
+            cmbRole.SelectedIndex = 1;
+            for (int i = 0; i < clbPermissions.Items.Count; i++)
+                clbPermissions.SetItemChecked(i, false);
+        }
+
+        private void lstUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lstUsers.SelectedIndex;
+            if (index < 0) return;
+
+            var user = users[index];
+            txtName.Text = user.Name;
+            cmbRole.SelectedItem = user.Role;
+            UpdatePermissionVisibility();
+
+            for (int i = 0; i < clbPermissions.Items.Count; i++)
+                clbPermissions.SetItemChecked(i, user.Permissions.Contains(clbPermissions.Items[i].ToString()));
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int index = lstUsers.SelectedIndex;
+            if (index < 0) return;
+
+            string name = txtName.Text.Trim();
+            string role = cmbRole.SelectedItem.ToString();
+            if (string.IsNullOrWhiteSpace(name)) return;
+
+            var updatedUser = new User
+            {
+                Name = name,
+                Role = role,
+                Permissions = new List<string>()
+            };
+
+            if (role == "User")
+            {
+                foreach (string permission in clbPermissions.CheckedItems)
+                    updatedUser.Permissions.Add(permission);
+            }
+            else
+            {
+                updatedUser.Permissions.Add("All Permissions");
+            }
+
+            users[index] = updatedUser;
+            RefreshUserList();
+            ClearForm();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = lstUsers.SelectedIndex;
+            if (index < 0) return;
+
+            users.RemoveAt(index);
+            RefreshUserList();
+            ClearForm();
+        }
+
+        private void cmbRole_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdatePermissionVisibility();
+        }
+    }
+}
